7c207b180c93bcd0c98013162d711e888622302e
meiamanja <anjanette.joven27@gmail.com>
2016-12-08 18:47:43 +0800

    update

--- views/application/deposit.php
+++ views/application/deposit.php
@@ -1,4 +1,4 @@
-<form id="frmDelivery" method="post" action="<?php echo URL;?>a_delivery">
+<form id="frmDeposit" method="post" action="<?php echo URL;?>a_deposit">
 	<input id = "hidUrl" type="hidden" name="hidUrl" value = "<?php echo URL;?>" />
 	<div class = 'divLoader'></div>
 	<div class = 'divContainerMain'>
@@ -10,61 +10,44 @@
 			<div class = "divContainerDetailsMenu">
 				<?php require 'views/menu/'.$this->user_role.'/submenu.php'; ?>
 			</div>
-			<div id = "divGridReportContainer"></div>
+			<div id = "divGridReportContainer">
+			</div>
 			<div class = "divFilterReportContainerBottom">
 				<input type="button" id = "btnExport" value = "Export to Excel"/>
 			</div>
 		</div>
 		
 		<div class = "divContainerInput">
-			<div class = "divContinerInputTitle">Delivery Information</div>
+			<div class = "divContinerInputTitle">Bank Deposit Details
+        </div>
 			<div class = "divContinerInputDetails">
-				<input type="hidden" name="hidDeliveryId" />
-				<input type="hidden" name="hidFromBranchId" value = "<?php echo $this->from_branch;?>" />
-				<input type="hidden" name="hidToBranchId" value = "<?php echo $this->to_branch;?>" />
-				<?php
-					if ($this->from_branch) {
-						echo '<label>Origin</label><input type="text" name="txtFromBranch" value = "'.$this->from_branch_name.'"/><br />';
-						echo '<input type="hidden" name="selFromBranch" value = "'.$this->from_branch.'" />';
-					} else {
-						echo '<label>Origin</label>
-							<select name="selFromBranch">
-							<option value=""></option>';
-						foreach($this->branch_list as $arrList) {
-							echo '<option value="'.$arrList['id'].'">'.$arrList['name'].'</option>';
-						}
-						echo '</select><br />';
-					}
-				?>
-				
-				<label>Delivery to</label>
-					<select name="selToBranch">
-					<option value=""></option>
-					<?php
-						foreach($this->branch_list as $arrList) {
-							echo '<option value="'.$arrList['id'].'">'.$arrList['name'].'</option>';
-						}
-					?>
-					</select><br />
-				<label>Delivery No</label><input type="text" name="txtDeliveryNo" /><br />
-				<label>Delivery Date</label><input type="text" name="txtDeliveryDate" class = "datepicker"/><br />
-				<label>Item</label>
-					<select name="selItemList">
-					<option value=""></option>
-					<?php
-						foreach($this->item_list as $arrList) {
-							echo '<option value="'.$arrList['id'].'">'.$arrList['name'].'</option>';
-						}
-					?>
-					</select><br />
-				<label>Quantity</label><input type="text" name="txtQty" /><br />
-				<label>Qty Receive</label><input type="text" name="txtQtyReceive" value = "0" readonly /><br />
-				<label>Imei</label><textarea name="txtaImei"/></textarea><br />
+				<input type="hidden" name="hidDepositId" />
+				<label>Date of Deposit</label><input type="text" name="txtDepositDate" class="datepicker"/><br />
+				<label>Account No</label><input type="text" name="txtAccountNo" /><br />
+				<label>Account Name</label><input type="text" name="txtAccountName" /><br />
+				<label>Deposit Amount</label><input type="text" name="txtDepositAmount" /><br />
+				<label>Deposited By</label><input type="text" name="txtDepositedBy" /><br />			
 			</div>
 			<div class = "divContinerInputButton">
-				<input type="submit" name = "subForm" id = "btnSaveAsNew" value = "Save as New"/>
-				<input type="button" id = "btnClear" value = "Clear"/>
-				<input type="submit" name = "subForm" id = "btnCancel" value = "Cancel"/>
+				<?php
+					switch ($this->user_role) {
+						case 'owner':
+						case 'admin':
+							echo '
+								<input type="submit" name = "subForm" id = "btnConfirm" value = "Confirm"/>
+								<input type="submit" name = "subForm" id = "btnDelete" value = "Delete"/>';
+							break;
+						case 'staff':
+						case 'supervisor':
+							echo '
+								<input type="submit" name = "subForm" id = "btnSaveAsNew" value = "Add Deposits"/>
+								<input type="button" id = "btnClear" value = "Clear"/>
+								<input type="submit" name = "subForm" id = "btnUpdate" value = "Update"/>
+								<input type="submit" name = "subForm" id = "btnCancel" value = "Cancel"/>';
+							break;
+							
+					}
+				?>
 			</div>
 		</div>
 	</div>
