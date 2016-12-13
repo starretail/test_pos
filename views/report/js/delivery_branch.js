9564c3578d557c7ed07f639f656f3b7c2208a837
hershelei <hershelei19@gmail.com>
2016-12-13 14:42:56 +0800

    16-12-13
    2.42pm

--- views/report/js/delivery_branch.js
+++ views/report/js/delivery_branch.js
@@ -11,10 +11,10 @@ $(function() {
 		
 		mygrid = new dhtmlXGridObject("divGridReportContainer");
 		mygrid.setImagesPath("codebase/imgs/"); 
-		mygrid.setHeader("Id,Origin,Delievery To,Delivery No,Delievery Date,Item,Qty Delivered,Qty Received,Status"); 
-		mygrid.attachHeader(",#select_filter_strict,#select_filter_strict,#text_filter,,#select_filter_strict,,,#select_filter_strict"); 
-		mygrid.setInitWidths("50,200,200,150,150,200,120,120,150");
-		mygrid.setColAlign("center,left,left,center,center,left,right,right,center");
+		mygrid.setHeader("Id,Origin,Delievery To,Delivery No,Delivery Date,Item,Qty Delivered,Qty Received,Status"); 
+		mygrid.attachHeader(",#select_filter_strict,#select_filter_strict,#text_filter,,#select_filter_strict,,#select_filter_strict"); 
+		mygrid.setInitWidths("50,200,200,150,200,200,100,100");
+		mygrid.setColAlign("center,left,left,center,center,left,right,center");
 		mygrid.setColTypes('ro,ro,ro,ro,ro,ro,ro,ro,ro');
 		mygrid.setSkin("light");
 		mygrid.init();
